c1e8539854b0132677484d51f7bd3c047bc889f9
fredwills9224 <willsf9224@gmail.com>
2022-02-23 15:23:25 -0500

    sup lo hs em npt + usrnm npt rfctr 2ts f

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,5 +1,11 @@
 <template>
+    
     <h1>Sign Up</h1>
-    <input placeholder="username"/>
-    <input placeholder="e-mail"/>
+
+    <label for="username">Username</label>
+    <input id="username"/>
+    
+    <label for="e-mail">E-mail</label>
+    <input id="e-mail"/>
+
 </template>
