dc4ce71e60105566f4113a5f35ba43f60dcb8dfa
fredwills9224 <willsf9224@gmail.com>
2022-02-22 19:49:48 -0500

    hs sgnUp hdr rfctr1 ts p

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,11 +1,12 @@
 import SignUpPage from './SignUpPage.vue';
 import { render, screen } from '@testing-library/vue';
+import '@testing-library/jest-dom';
 
 
 it('has Sign Up Header', ()=>{
 
     render(SignUpPage);
     const header = screen.queryByRole('heading', { name: 'Sign Up' });
-    expect(header).not.toBeNull();
+    expect(header).toBeInTheDocument();
 
 });
